beba3a9e3d925304d276ff9eba250342f3253a5b
Thomas Monvoisin <thomasmonvoisin@gmail.com>
2020-12-14 19:34:16 +0100

    added index and new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,29 @@
 class UsersController < ApplicationController
 
+  def new
+    @user = User.new
+  end
+
+  def add_user
+    @password = 'password'
+    @user = User.new(user_params)
+    @user.password = @password
+    @user.save
+    if @user.save
+      redirect_to '/users'
+    else
+      render 'new'
+    end
+  end
+
+
   def index
     # authorize :user, :manager?
     @users = User.all
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :address, :manager, :fixer)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   resources :users, only: [:new, :index, :destroy]
-
+  post 'users/add_user', to: 'users#add_user'
 end
